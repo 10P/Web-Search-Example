fea5cca0483db318d56223ba40f9ca2bdc830b30
E <fello@Floresas-MacBook-Pro.local>
2015-03-15 17:05:21 -0400

    Began implementing search

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET("https://api.instagram.com/v1/tags/cats/media/recent?client_id=099c1261712d435f956f15c5bda7aa27", parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+                
+                if let dataArray = responseObject["data"] as? [AnyObject] {
+                    var urlArray:[String] = []
+                    for dataObject in dataArray {
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            urlArray.append(imageURLString)
+                        }
+                    }
+                    self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    
+                    for var i = 0; i < urlArray.count; i++ {
+                            let imageView = UIImageView(frame: CGRectMake(0, 320 * CGFloat(i), 320, 320))
+                            imageView.setImageWithURL(NSURL(string: urlArray[i]))
+                            self.scrollView.addSubview(imageView)
+                        }
+                    }
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+            }
+        
+        )
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +51,12 @@ class ViewController: UIViewController {
     }
 
 
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        searchBar.resignFirstResponder()
+    }
 }
 
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by E on 3/15/15.
+//  Copyright (c) 2015 punch. All rights reserved.
+//
+
+#ifndef Photo_Search_Example_bridge_h
+#define Photo_Search_Example_bridge_h
+#import "AFNetworking/AFNetworking.h"
+#import "UIKit+AFNetworking/UIKit+AFNetworking.h"
+
+
+#endif
